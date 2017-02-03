e5987805f20cda320fb37cc15acc56bae099144f
shashisadasivan <shashi.sadasivan@gmail.com>
2017-02-03 19:27:48 +1300

    Initial basic working code
    Working code for Arduino and C# for creating the clicks

---
+++ csharp/ArduinoUnoMouse/ArduinoUnoMouse/Main.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ArduinoUnoMouse
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.serialPortArduino = new System.IO.Ports.SerialPort(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // serialPortArduino
+            // 
+            this.serialPortArduino.PortName = "COM3";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "Hello baloon";
+            this.notifyIcon.BalloonTipTitle = "Baloon title";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Main";
+            this.Text = "Arduino Uno Mouse";
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPortArduino;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ csharp/ArduinoUnoMouse/ArduinoUnoMouse/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoUnoMouse
+{
+    public partial class Main : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.serialPortArduino.Open();
+            this.serialPortArduino.DataReceived += SerialPortArduino_DataReceived;
+        }
+
+        private void SerialPortArduino_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (e.EventType == SerialData.Chars)
+            {
+                SerialPort port = sender as SerialPort;
+                var data = port.ReadLine();
+                Console.WriteLine(data);
+                data = data.TrimEnd(System.Environment.NewLine.ToCharArray());
+                if (data.Equals("click"))
+                {
+                    Console.WriteLine("clicking");
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                }
+            }
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIcon.Visible = false;
+        }
+    }
+}
